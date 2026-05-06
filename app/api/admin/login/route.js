2639119c06399f2b1ee3ba04063c7e27e810bbc2
ahmaddhr <ahmaddaherr004@gmail.com>
2026-05-07 02:47:45 +0300

    debug done

--- app/api/admin/login/route.js
+++ app/api/admin/login/route.js
@@ -15,8 +15,6 @@ export async function POST(req) {
     return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
   }
 
-  console.log("SECRETTTT" ,process.env.JWT_SECRET)
-
   const token = jwt.sign(
     { id: admin._id, adminname: admin.adminname },
     process.env.JWT_SECRET,
